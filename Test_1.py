be88035c9b7a69dcf50ce0839d9e8b3a21f13ea9
chh4031 <chh4031@gmail.com>
2023-06-05 18:28:11 +0900

    가장 잘 나온 예시

---
+++ Test_1.py
@@ -0,0 +1,81 @@
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.callbacks import LearningRateScheduler
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import tensorflow as tf
+
+seed = 0
+np.random.seed(seed)
+tf.random.set_seed(3)
+
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32') / 255
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32') / 255
+Y_train = np_utils.to_categorical(Y_train)
+Y_test = np_utils.to_categorical(Y_test)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(28, 28, 1), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+
+MODEL_DIR = './model/'
+if not os.path.exists(MODEL_DIR):
+    os.mkdir(MODEL_DIR)
+
+modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+checkpointer = ModelCheckpoint(filepath=modelpath, monitor='val_loss', verbose=1, save_best_only=True)
+early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10)
+
+datagen = ImageDataGenerator(
+    rotation_range=10,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1
+)
+datagen.fit(X_train)
+
+def lr_scheduler(epoch):
+    if epoch < 10:
+        return 0.001
+    else:
+        return 0.001 * np.exp(0.1 * (10 - epoch))
+
+lr_schedule = LearningRateScheduler(lr_scheduler)
+
+history = model.fit_generator(datagen.flow(X_train, Y_train, batch_size=200),
+                              steps_per_epoch=len(X_train) // 200,
+                              validation_data=(X_test, Y_test),
+                              epochs=30,
+                              verbose=1,
+                              callbacks=[early_stopping_callback, checkpointer, lr_schedule])
+
+print("\nTest Accuracy: %.4f" % (model.evaluate(X_test, Y_test)[1]))
+
+y_vloss = history.history['val_loss']
+y_loss = history.history['loss']
+x_len = np.arange(len(y_loss))
+plt.plot(x_len, y_vloss, marker='.', c="red", label='Testset_loss')
+plt.plot(x_len, y_loss, marker='.', c="blue", label='Trainset_loss')
+plt.legend(loc='upper right')
+plt.grid()
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
+
+# history 부분에서 학습률 조정, 데이터 증강 추가버전
+# 결과 : 0.9948
